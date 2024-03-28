637d1dfa2cb09e3b6a1880b9671f96daccd3fa46
DomEspeleta0223 <163530079+DomEspeleta0223@users.noreply.github.com>
2024-03-28 20:50:14 +0800

    Initial commit

---
+++ Espeleta_Assignment _Item_Number_2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+internal class Espeleta_Assignment_Item_Number_2
+{
+    static void Main(string[] args)
+    {
+        int N = 1;
+        while (N < 101)
+        {
+            if (N % 3 == 0 && N % 5 == 0)
+            {
+                Console.WriteLine("Hello Goodbye");
+            }
+            else if (N % 3 == 0)
+            {
+                Console.WriteLine("Hello");
+            }
+            else if (N % 5 == 0)
+            {
+                Console.WriteLine("Goodbye");
+            }
+            else
+            {
+                Console.WriteLine(N);
+            }
+            N++;
+        }
+    }
+
+}
